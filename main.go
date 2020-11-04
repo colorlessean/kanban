f352d4a4c88cfe77eeea56148b8f3638f840fdca
Sean O'Sullivan <seanosullivan646@gmail.com>
2020-11-03 20:56:56 -0600

    Added some inital code to test api building
    Signed-off-by: Sean O'Sullivan <seanosullivan646@gmail.com>

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import(
     "fmt"
+    "log"
+    "net/http"
 )
 
+func boardHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/api/board" {
+        http.Error(w, "404 not found", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Method is not supported.", http.StatusNotImplemented)
+        return
+    }
+
+    log.Println(w, "Board Handler")
+}
+
 func main() {
-    fmt.Println("Environment is Working!")
+    // allows the for importing of the static assets
+    fileServer := http.FileServer(http.Dir("./static"))
+    http.Handle("/", fileServer)
+    http.HandleFunc("/api/board", boardHandler)
+
+    fmt.Printf("Starting Server at port 8080\n")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
