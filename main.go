828488962befd168031cf9b503044a3abf02c12a
Sean O'Sullivan <seanosullivan646@gmail.com>
2020-11-07 22:02:42 -0600

    Added additional handlers and the beggining of the structure of the business logic

--- main.go
+++ main.go
@@ -6,6 +6,52 @@ import(
     "net/http"
 )
 
+// Main datatypes for the api and the baord itself
+// idea is that tickets are contained within columns contained within boards
+// This is to be kept simple at first with complexity growing over time
+type Ticket struct {
+    title string
+    body string
+}
+
+type Column struct {
+    title string
+    tickets []Ticket
+}
+
+type Board struct {
+    title string
+    columns []Column
+}
+
+func ticketHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/api/ticket" {
+        http.Error(w, "404 not found", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Method is not supported", http.StatusNotImplemented)
+        return
+    }
+
+    fmt.Println("Ticket Handler")
+}
+
+func columnHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/api/column" {
+        http.Error(w, "404 not found", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Method is not supported", http.StatusNotImplemented)
+        return
+    }
+
+    fmt.Println("Column Handler")
+}
+
 func boardHandler(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/api/board" {
         http.Error(w, "404 not found", http.StatusNotFound)
@@ -22,9 +68,11 @@ func boardHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     // allows the for importing of the static assets
-    fileServer := http.FileServer(http.Dir("./static"))
+    fileServer := http.FileServer(http.Dir("./web"))
     http.Handle("/", fileServer)
     http.HandleFunc("/api/board", boardHandler)
+    http.HandleFunc("/api/column", columnHandler)
+    http.HandleFunc("/api/ticket", ticketHandler)
 
     fmt.Printf("Starting Server at port 8080\n")
     if err := http.ListenAndServe(":8080", nil); err != nil {
